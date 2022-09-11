df7631a1bdbbeadcd14599f95e65250934c1e2f2
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-09-11 10:50:19 +0530

    Create even_number_in_array.c

---
+++ even_number_in_array.c
@@ -0,0 +1,36 @@
+// WAP to print all odd numbers in arrray...
+
+#include <stdio.h>
+
+int even(int[], int);  // This is function declaration.
+int i;
+int main() 
+{
+    int n;
+    printf("Enter the number of elements : ");
+    scanf("%d", &n);
+    int a[n];
+    for(i=0; i<n; i++)
+    {
+        printf("Enter the %dth element : ", i);
+        scanf("%d", &a[i]);
+    }
+    even(a, n);
+    return 0;
+}
+
+int even(int a[], int size)
+{
+    // int j;
+    for(i=0; i<size; i++)
+    {
+        if(a[i]%2==0)
+        {
+            printf("%d is even number..\n", a[i]);
+        }
+    }
+    // for(j=0; j<cnt; j++)
+    // {
+    //     printf("%d\t", even[j]);
+    // }
+}
