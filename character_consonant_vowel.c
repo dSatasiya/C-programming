2dd4de9f120e85da829993faf4fe030a9330ec0c
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2022-09-19 10:36:27 +0530

    Create character_consonant_vowel.c

---
+++ character_consonant_vowel.c
@@ -0,0 +1,19 @@
+// WAP to check whether given character is vowel or consonant.
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Enter the character : ");
+    scanf("%c", &ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        printf("%c is vowel...", ch);
+    }
+    else
+    {
+        printf("%c is consonant...", ch);
+    }
+    return 0;
+}
