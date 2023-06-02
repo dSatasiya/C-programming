854a10fc077c90606b1384d6a6ecf789b9dfcabb
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2023-06-02 19:05:49 -0400

    Update Sleep_Function_for_Delay.c

--- Sleep_Function_for_Delay.c
+++ Sleep_Function_for_Delay.c
@@ -1,19 +1,34 @@
-// This is the code in C about how to include the delay between the execution of the two lines.
+// //===================================
+//Author: Dhruv Satasiya
+//Copyright: 2023 Fanshawe College
+//Date: June 2, 2023
+//Description: Practice of different libraries and random number generator..
+//=================================
 
 #include<stdio.h>
 #include<windows.h>
+#include<time.h>
 
 int main()
 {
-	printf("Hello, I want to sleep for 5 seconds....\n");
-	Sleep(5000);  // Sleep(time_in_milisecond) and note S is capital.
-	printf("Wake up, your 5 seconds are done...\n");
+	printf("Hello, I want to sleep for 2 seconds....\n");
+	Sleep(2000);  // Sleep(time_in_milisecond) and note S is capital.
+	printf("Wake up, your 2 seconds are done...\n");
 	
 	int i;
 	for (i = 0; i < 50; i++)
 	{
 		printf("%d\n", i);
-		Sleep(50);   // Delay of 0.5 seconds before printing the next number...
+		Sleep(50);   // Delay of 0.05 seconds before printing the next number...
 	}
+
+	//time_t timer = time(NULL);
+	//printf("Current time is %s", ctime(&timer));
+
+	time_t seconds;     // Remmber time_t is nothing but a data type like int.
+	seconds = time(NULL);
+
+	printf("%d\n", seconds);
+
 	return 0;
 }
