e650b1c90460a9fed998ab1221e0b2bbf9c85a7d
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2023-06-22 15:58:24 -0400

    Create pattern_ractangle.c

---
+++ pattern_ractangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 0;
+    int j;
+    for(j=0; j<5; j++)
+    {
+        int i = 0;
+        while (i<5)
+        {
+            printf("*\t");
+            i++;
+        }
+        printf("\n");
+    }
+    
+    printf("\n");
+    return 0;
+    
+}
