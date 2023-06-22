043fe4b79860a5283ba23c18e1732c832fc130a0
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2023-06-22 15:59:07 -0400

    Create pattern_right_angle.c

---
+++ pattern_right_angle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    int modifier = 1;
+    int totalLines;
+    printf("Enter the total number of lines to make the right angle: ");
+    scanf("%d", &totalLines);
+    for(i = 0; i<totalLines; i++)
+    {
+        int j = 0;
+        while(j<modifier)
+        {
+            printf("*\t");
+            j++;
+        }
+        modifier = modifier + 1;
+        printf("\n");
+    }   
+    return 0;
+}
