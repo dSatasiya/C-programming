307ea4931b6cfba7839337a469b29369551ef0e7
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2023-07-11 15:51:51 -0400

    Update 6006LAB7_string_and_switch.c

--- 6006LAB7_string_and_switch.c
+++ 6006LAB7_string_and_switch.c
@@ -2,8 +2,8 @@
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 	File Name:	ELNC6006LAB7DS.c  <-- Change this according to conventions.
 	Author:		Dhruv Satasiya
-	Date:		04/07/2023
-	Modified:	04/07/2023
+	Date:		July 11. 2023
+	Modified:	None
 	© Fanshawe College, 2023
 
 	Description: Promting the user to enter a sentence and performing some mathematical
@@ -24,47 +24,43 @@ int main(void)	// <--- lists an int return type and no input arguments (void).
 {
 	// code for main function here
 	char choice = NULL;
-
 	char stringArray[STRLEN];
+
 	while (choice != 'q' && choice != 'Q')
 	{
+		printf("\n");
+		printf("\n1. To enter the sentence");
+		printf("\n2. To perform math operation");
 		printf("\nEnter your choice or enter 'q' or 'Q' to exit : ");
-		scanf(" %c", &choice);         // what buffer clearing ?
+		scanf(" %c", &choice);     
+
 		int counter = 0;
 
 		switch (choice)
 		{
-		case '1': printf("\nEnter the sentence : ");
-			scanf("%s", stringArray);
-			printf("%s", stringArray);   // Why is it not printing properly...
-			break;
+			case '1': 
+				printf("\nEnter the sentence : ");           // Prompting the user for the sentence..
+				scanf("%s", &stringArray, STRLEN);
+				//printf("%s", stringArray);   
+				break;
 
-		case '2':
-			while (stringArray[counter] != '\0')
-			{
-				if (stringArray[counter] != '\0')
+			case '2':
+				printf("Performing 2's Compliment...");     // Performing the math operation
+				while (stringArray[counter] != '\0')
 				{
+					stringArray[counter] = ~(stringArray[counter]);  // Applying 2's Compliment
+					stringArray[counter] = stringArray[counter] + 1;
 					counter++;
-				}  // end of if 
-			}// end of while
-			printf("Counter is : %d", counter);
-			break;
+				}// end of while loop
+				printf("\nCounter is : %d", counter);
+				printf("\nThe result is : %s", stringArray);
+				break;
 
-		case '3': printf("Performing 2's Compliment...");
-			while(stringArray[counter] != '\0')
-			{
-				stringArray[counter] = ~(stringArray[counter]);
-				stringArray[counter] = stringArray[counter] + 1;
-				counter++;
-			}// end of while
-			printf("\nThe result is : %s", stringArray);
-			break;
+			default:
+				break;
+		}// end of switch
 
-		default:
-			break;
-		}
-
-	}
+	}// end of while loop
 
 	return 0;	// returning argument in the main function is listed as an int, 
 	// this 'return' command returns a value of 0 which is of int type
