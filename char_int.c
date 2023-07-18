0cb8dc2bb2b6c5e0c255d5fd2301d9d690c4a80a
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2023-07-18 19:08:30 -0400

    Create char_int.c

---
+++ char_int.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	char num1 = 10;
+	char num2 = 40;
+	int sum = 0;
+
+	//num1 = num1 - 2;
+	sum = num1 + num2;
+
+	printf("Your sum is : %d", sum);
+	/*if (typeof(num1) == typeof(sum))
+	{
+		printf("Both the variables have same type...");
+	}*/
+	//printf(type(sum));
+
+	return 0;
+}
