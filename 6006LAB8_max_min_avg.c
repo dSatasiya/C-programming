c58947ab548b32f737f8efe600820f3eadadc8f8
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2023-07-25 16:08:26 -0400

    Update 6006LAB8_max_min_avg.c

--- 6006LAB8_max_min_avg.c
+++ 6006LAB8_max_min_avg.c
@@ -6,11 +6,8 @@
 	Modified:	None
 	© Fanshawe College, 2023
 
-	Description: 	Describe the purpose of the main function.  What the program
-			does and why. What platform (hardware part number) and / or
-			supplemental documents required for the program’s use.
-			Describe any supporting information (schematic, programming
-			environment, ect..).
+	Description: This code will prompt the user for the grades and perform some mathamatical operations
+				 on it like minimum value, maximum value and average. 
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 
 // ANY and ALL comments that are not relevant to the program should be removed, including this one.
@@ -24,47 +21,46 @@
 #define COUNT 10
 
 // Global Variables >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-char myGrades[COUNT] = { 0 };
-int index;
+
+
 // Functions >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 	// >>> Function Header >>>
 /*>>> functionName: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-Author:		Satasiya Dhruv 
+Author:		Satasiya Dhruv
 Date:		18/07/2023
 Modified:	None
-Desc:		Specifically, what does this function do?
-Input: 		None or dataType variableName, why this input is needed / what is its use
-Returns:	None or datatype, what is the data / what is its meaning
+Desc:		This function calculates the minimum, maximum and average of the grades.
+Input: 		It takes the address of the grades array.
+Returns:	It returns the calculated minimum, maximum and average of the grades.
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-int calculateScore(void)
+int calculateScore(char* ptr)   // function to clculate the min, max and the average of the student score.
 {
-	char minVal = myGrades[0];
-	char maxVal = myGrades[0];
+	char minVal = *ptr;
+	char maxVal = *ptr;
 	int sum = 0;
 	float average = 0.0f;
-	char* ptr;
+	int index;
+	//char* ptr;
 	for (index = 0; index < COUNT; index++)
 	{
-		ptr = &myGrades[index];     // Assigning the address of value every time to the character pointer.
 
-		if (*ptr > maxVal)
+		if (*ptr > maxVal)    // Calculating the max
 		{
 			maxVal = *ptr;
 		}
-		if (*ptr < minVal)
+		if (*ptr < minVal)    // Calculating the min
 		{
 			minVal = *ptr;
 		}
 		sum = sum + *ptr;
+		ptr++;
 	}
-	average = sum / (float)COUNT;             // Not converting COUNT into float will give integer value
-											  // of the average.
+	average = sum / COUNT;    // Calculating the average
 	//return maxVal, minVal, sum, average;
 	printf("\nThe minimum value is : %d", minVal);
 	printf("\nThe maximum value is : %d", maxVal);
-	printf("\nThe Sumation is : %d", sum);
-	printf("\nThe Average is : %.2f", average);
+	printf("\nThe Average is : %d", average);
 	printf("\n");
 
 } // eo functionName::
@@ -76,6 +72,7 @@ int main(void)	// <--- lists an int return type and no input arguments (void).
 {
 
 	char choice = '\0';
+	char myGrades[COUNT] = { 0 }; // Creating the grades array
 
 	while (choice != 'q' && choice != 'Q')
 	{
@@ -88,16 +85,16 @@ int main(void)	// <--- lists an int return type and no input arguments (void).
 
 		int input;
 		//char* ptr;
-
+		int index;
 		switch (choice)
 		{
-		case 'A':
+		case 'A':                                      // This case will prompt the user to fill the grades into the array.
 			printf("Enter the test scores between 0~100: ");
 			for (index = 0; index < COUNT; index++)
 			{
 				scanf_s("%d", &myGrades[index]);
 				input = myGrades[index];
-				while (input < 0 || input > 100)
+				while (input < 0 || input > 100)        // Checking the range of the input between 0~100
 				{
 					printf("Invalid Input, Enter again : ");
 					scanf_s("%d", &myGrades[index]);
@@ -107,10 +104,10 @@ int main(void)	// <--- lists an int return type and no input arguments (void).
 			break;
 
 		case 'B':
-			calculateScore();
+			calculateScore(myGrades);					 //Calling the function to calculate the min, max and average
 			break;
 
-		default:
+		default:									    // Default case for the invalid user input.
 			printf("\nYou have made an invalid choice....");
 			printf("\n");
 			break;
@@ -118,6 +115,6 @@ int main(void)	// <--- lists an int return type and no input arguments (void).
 	}
 
 	return 0;	// returning argument in the main function is listed as an int, 
-	// this 'return' command returns a value of 0 which is of int type
-	// and is required in Visual Studio C++ source codes. 
-} // eo main:
+				// this 'return' command returns a value of 0 which is of int type
+				// and is required in Visual Studio C++ source codes. 
+} // eo main::
