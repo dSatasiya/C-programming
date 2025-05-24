d484bbae265d3b3df6616861a43cb504443ff48a
Dhruv Satasiya <100504799+dSatasiya@users.noreply.github.com>
2025-05-24 19:28:47 -0400

    Create pointer_basics.c

---
+++ Pointers/pointer_basics.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    // int a = 10;
+    // int *p;
+    // p = &a;
+
+    // printf("%d", *p);
+
+    // Home work problem ....
+
+    int i = 1;
+    int *p = &i;
+    // int q, p;
+    // q = p;
+    int *q = 5;
+    printf("%d\n", i);
+    printf("%d\n", &i);
+    printf("%d\n", *p);
+    printf("%d\n", p);
+    // printf("%d", *p);
+}
